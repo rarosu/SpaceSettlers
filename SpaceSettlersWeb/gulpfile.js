1b0a5b93f6628e47d8cbc758666cc0c9ba0b8395
Lars Woxberg <lars.woxberg@gmail.com>
2016-01-12 11:20:46 +0100

    Started on a gulp-based task system.
    Also npm for managing dependencies. Gulp will be used to copy dependencies
    into the public_html directory.

---
+++ SpaceSettlersWeb/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+
+gulp.task('copy-dependencies', function() {
+    gulp.src('node_modules/requirejs/require.js')
+        .pipe(gulp.dest('public_html/js/External'));
+});
+
+gulp.task('dev', ['copy-dependencies'], function() {
+    // TODO: Add dependencies for tasks that will be performed while developing.
+});
+
+gulp.task('deploy', ['copy-dependencies'], function() {
+    // TODO: Add dependencies for tasks needed to prepare the site for deployment.
+});
+
+gulp.task('default', ['dev'], function () {
+    // Running just 'gulp' should run the dev tasks.
+});
